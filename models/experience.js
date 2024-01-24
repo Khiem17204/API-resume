183a3c4cf7ce0009ab10fe5c15ca49a687f675c6
Khiem Le <87161814+Khiem17204@users.noreply.github.com>
2024-01-24 23:33:27 +0700

    test experience + connection to database

--- models/experience.js
+++ models/experience.js
@@ -1,10 +1,13 @@
 const {Schema, model} = require("mongoose")
-
+const db = require("../config/connection")
+db.once('open', () => {
+    console.log('Connected to the database1');
+});
 const experienceSchema = new Schema(
     {
         title: String,
         company: String,
-        duration: {startDate: Date, endDate: Date},
+        duration: {startDate: String, endDate: String},
         current: Boolean,
         description: String,
         responsibilities: Array
--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 const db = require("./config/connection")
+const {createExperience, getExperience} = require("./controllers/experience.controller")
 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+db.once('open', () => {
+    console.log('Connected to the database');
+});
+app.post('/experience', createExperience)
+app.get("/a", getExperience)
   
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
